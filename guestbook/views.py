926d58e6c05c8b0d96d1527c569b083b9e427a82
Kuba <ksulaymanov@gmail.com>
2022-07-02 23:03:15 -0500

    filtered to show active notes byt last date

--- guestbook/views.py
+++ guestbook/views.py
@@ -5,6 +5,6 @@ from guestbook.models import Book
 
 
 def index_view(request):
-    notes = Book.objects.order_by('-created_at')
+    notes = Book.objects.order_by('-created_at').filter(status='active')
     context = {'notes': notes}
     return render(request, 'index.html', context)
