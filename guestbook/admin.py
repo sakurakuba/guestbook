c5e040a0656ffc962a73e7e6701b72c344df85b0
Kuba <ksulaymanov@gmail.com>
2022-07-02 21:41:55 -0500

    added admin and registered model there

--- guestbook/admin.py
+++ guestbook/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from guestbook.models import Book
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'created_at', 'status']
+    list_display_links = ['author']
+    list_filter = ['author']
+    search_fields = ['author', 'content']
+    fields = ['author', 'email', 'content', 'created_at', 'updated_at', 'status']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(Book, BookAdmin)
